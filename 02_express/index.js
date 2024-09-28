3c37aeeec88ba71e7683d3f5649fbeaf781cdfdb
ChandraShekhar <csyadav0513@gmail.com>
2024-09-28 21:23:55 +0530

    Refactor code to use ES6 import syntax for Express

--- 02_express/index.js
+++ 02_express/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/babu", (req, res) => {
+  res.send("Hello Babu Boi");
+});
+
+app.get("*", (req, res) => {
+  res.send("Not Found 404");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
