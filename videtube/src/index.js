9ac1d2656b90806d80156fb244d096d640036eee
ChandraShekhar <csyadav0513@gmail.com>
2024-10-01 20:46:18 +0530

    Update package.json and index.js

--- videtube/src/index.js
+++ videtube/src/index.js
@@ -1,6 +1,11 @@
-import { app } from "./app.js";
+import dotenv from "dotenv";
+import { app } from "./app.js"; 
 
-const PORT = process.env.PORT || 3000;
+dotenv.config({
+    path: "./.env",
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
