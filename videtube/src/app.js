838b146bc3b1d9cb0a1e110c9db75af7995b196b
ChandraShekhar <csyadav0513@gmail.com>
2024-10-01 20:56:08 +0530

    Add CORS middleware and common middleware to app.js

--- videtube/src/app.js
+++ videtube/src/app.js
@@ -1,6 +1,26 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+//! Common Middleware
+
+//? Allow CORS for all routes in the app
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//? We can accept JSON in the body of the request
+app.use(express.json({ limit: "16kb" }));
+
+//? Allow URL encoded data in the body of the request
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+//? Serve static files from the public directory
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
