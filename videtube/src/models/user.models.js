5ced533e200178f6846f535202c2c17e580ef773
ChandraShekhar <csyadav0513@gmail.com>
2024-10-02 00:25:53 +0530

    Add User model for storing user information

---
+++ videtube/src/models/user.models.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+import { required } from "nodemon/lib/config";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // coludinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // coludinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
