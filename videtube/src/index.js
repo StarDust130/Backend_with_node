94e5f60a49282ec7fc44feb7a424b4fbbd300540
ChandraShekhar <csyadav0513@gmail.com>
2024-10-01 20:40:17 +0530

    Add express server and listen on specified port

--- videtube/src/index.js
+++ videtube/src/index.js
@@ -1,2 +1,7 @@
-console.log("Hello World from ChandraShekhar");  
+import { app } from "./app.js";
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
