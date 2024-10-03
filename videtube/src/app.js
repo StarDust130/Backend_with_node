2a5e9885bd4437b5d6ce3592c67997309a6e4dbb
ChandraShekhar <csyadav0513@gmail.com>
2024-10-03 14:25:33 +0530

    Update dependencies and add cookie-parser package

--- videtube/src/app.js
+++ videtube/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(
     credentials: true,
   })
 );
+
+//? Parse cookies from the request headers
+app.use(cookieParser());
 
 //? We can accept JSON in the body of the request
 app.use(express.json({ limit: "16kb" }));
