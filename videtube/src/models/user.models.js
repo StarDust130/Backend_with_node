9072c0ad0f25348aa57bfd9be170e5b38da55d00
ChandraShekhar <csyadav0513@gmail.com>
2024-10-03 14:01:12 +0530

    Add jsonwebtoken package for JWT token generation in User model

--- videtube/src/models/user.models.js
+++ videtube/src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -48,11 +49,33 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+//! Hash the password before saving the user
 userSchema.pre("save", async function (next) {
   if (!this.modified("password")) next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+//! Compare the password with the hashed password
+// this is we created to check the password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  // short lived acess token
+  jwt.sign(
+    {
+      _id: this._id, // data to be stored in the token
+      email: this.email,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, // secret key
+    {
+      expiresIn: "15m", //Expires in 15 minutes
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
