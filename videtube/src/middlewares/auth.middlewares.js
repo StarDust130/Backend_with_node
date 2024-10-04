ab3468471d46af2b5bf46136060955cbf649394c
ChandraShekhar <csyadav0513@gmail.com>
2024-10-04 12:13:09 +0530

    Refactor auth middleware to include JWT verification and user retrieval

---
+++ videtube/src/middlewares/auth.middlewares.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  // 1) Get the token from the request
+  const token = req.headers.authorization;
+
+  // 2) Check if the token exists
+  if (!token) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  // 3) Verify the token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  // 4) Check if the user exists
+  const user = await User.findById(decoded.id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // 5) Attach the user to the request object
+  req.user = user;
+
+  next();
+});
