5746d31841fd487840dfc3f3431de72a445d91e1
ChandraShekhar <csyadav0513@gmail.com>
2024-10-04 21:54:09 +0530

    add middlerware to route

--- videtube/src/routes/user.routes.js
+++ videtube/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
@@ -12,5 +13,8 @@ router.route("/register").post(
 
   registerUser
 );
+
+// secure route
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
