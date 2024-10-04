686093a5aa41c42739936845b1da188a0c21a82f
ChandraShekhar <csyadav0513@gmail.com>
2024-10-04 11:34:52 +0530

    Refactor user controller to handle file uploads and use async/await syntax

--- videtube/src/controllers/user.controller.js
+++ videtube/src/controllers/user.controller.js
@@ -6,6 +6,69 @@ import {
   uploadOnCloudinary,
 } from "../utils/cloudinaray.js";
 import { ApiError } from "next/dist/server/api-utils/index.js";
+import jwt from "jsonwebtoken";
+
+//! Generate a new access & refresh token
+const generateAccessAndRefreshToken = async (userID) => {
+  try {
+    // 1)Find and validate the user
+    const user = await User.findById(userID);
+
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
+
+    // 2) Generate a new access  & refresh token
+    const acessToken = user.generateAccessToken();
+    const refershToken = user.generateRefreshToken();
+
+    // 3) Save the refresh token to the user document
+    user.refreshToken = refershToken;
+    await user.save();
+
+    return { acessToken, refershToken };
+  } catch (error) {
+    throw new ApiError(500, "Failed to generate access & refresh token");
+  }
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+  // 1) Accept the data from the user
+  const { email, username, password } = req.body;
+
+  // 2) Validate the data
+  if (
+    ![email, password, username].includes(undefined) ||
+    [email, password, username].includes("")
+  ) {
+    throw new ApiError(400, "Please provide all the required fields", res);
+  }
+  // 3) Check if the user exists
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found", res);
+  }
+
+  // 4) Check if the password is correct
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid Credentials", res);
+  }
+  // 5) Generate a new access & refresh token
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  // 6) Send back the response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { accessToken }, "Login Successful 🎉"));
+});
 
 const registerUser = asyncHandler(async (req, res) => {
   // 1) Accept Data from the user
@@ -103,4 +166,60 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  // 1) Accept the refresh token
+  const { refreshToken } = req.cookies.refreshToken || req.body.refreshToken;
+
+  // 2) Validate the refresh token
+  if (!refreshToken) {
+    throw new ApiError(400, "Refresh Token is required", res);
+  }
+
+  // 3) Verify the refresh token
+  try {
+    const decodedToken = jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, user) => {
+        if (err) {
+          throw new ApiError(403, "Invalid Refresh Token", res);
+        }
+
+        const user = await User.findById(decodedToken?._id);
+
+        if (!user) {
+          throw new ApiError(401, "Invalid refersh Token", res);
+        }
+
+        if (refreshToken !== user?.refreshToken) {
+          throw new ApiError(401, "Invalid refresh Token 😱", res);
+        }
+
+        // 3) Generate a new access token
+        const accessToken = jwt.sign(
+          { id: user.id },
+          process.env.ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: "15m",
+          }
+        );
+
+        // 4) Send back the new access token
+        return res
+          .status(200)
+          .json(
+            new ApiResponse(200, { accessToken }, "Access Token Refreshed 🎉")
+          );
+      }
+    );
+  } catch (error) {
+    throw new ApiError(500, "Failed to refresh access token", res);
+  }
+});
+
+export {
+  registerUser,
+  loginUser,
+  refreshAccessToken,
+  generateAccessAndRefreshToken,
+};
